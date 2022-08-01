abf37784dc0e3e4400e3a892928deeb8696c78c7
dryDeveloper <jesus.humble.coder@gmail.com>
2022-07-31 23:58:53 -0700

    separate module for material components

--- WebUI/src/app/app.module.ts
+++ WebUI/src/app/app.module.ts
@@ -6,11 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
@@ -22,11 +18,7 @@ import { MatListModule } from '@angular/material/list';
     AppRoutingModule,
     BrowserAnimationsModule,
     LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [MainMenuComponent]
